702c294b83052d9ac281506e2afd9142acca8858
rafael.cavaletti <rheffercavaletti@gmail.com>
2015-09-22 02:40:08 -0300

    dev update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World!!!");
-  response.write("<br/> new version");
+  response.write("<br/> new version - NEW NEW, but dev dev");
   response.end();
   console.log("##logTest##");
 }).listen(process.env.PORT || 8888);
