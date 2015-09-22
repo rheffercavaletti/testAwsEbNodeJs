475d5b9dc0dadf08b8a6a1537dadc7eac7df6fe7
rafael.cavaletti <rheffercavaletti@gmail.com>
2015-09-22 02:16:05 -0300

    new version

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World!!!");
+  response.write("<br/> new version");
   response.end();
+  console.log("##logTest##");
 }).listen(process.env.PORT || 8888);
